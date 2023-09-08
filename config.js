e09a997942e51df051b3f61215ede377aa95ad4b
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-09-08 09:37:58 +0000

    first1

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URL: `mongodb+srv://founaghe:founaghe@cluster0.ly68n1b.mongodb.net/posts?retryWrites=true&w=majority`
+}
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors')//added
+const { MONGO_URL } = require("./config");
+const postsRoutes = require("./routes/api/posts_controller");
+const app = express();
+
+app.use(cors())//added
+
+
+app.use(express.json());
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected!"))
+  .catch((err) => console.log(err));
+
+app.use("/api/posts", postsRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
