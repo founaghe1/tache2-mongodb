fc0a0768f93d29519bee86b47ac980a028e34a12
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-09-09 00:59:35 +0000

    port10000

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ mongoose.connect(MONGO_URL, {
 
 app.use("/api/posts", postsRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
